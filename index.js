a0ac1a43dfd345480cdc133009f389a50e707e2b
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-03-04 16:50:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,69 @@
 // Your code here
+function createEmployeeRecord(employee) {
+  const [firstName, familyName, title, payPerHour] = employee;
+  return {
+    firstName,
+    familyName,
+    title,
+    payPerHour,
+    timeInEvents: [],
+    timeOutEvents: []
+  };
+}
+
+function createEmployeeRecords(sourceArray) {
+  return sourceArray.map(employee => { return createEmployeeRecord(employee) });
+}
+
+function createTimeInEvent(employeeRecord, dateTime) {
+  const record = Object.assign({}, employeeRecord);
+
+  record.timeInEvents.push({
+    type: "TimeIn",
+    hour: parseInt(dateTime.split(" ")[1]),
+    date: dateTime.split(" ")[0]
+  });
+  return record;
+}
+
+function createTimeOutEvent(employeeRecord, dateTime) {
+  const record = Object.assign({}, employeeRecord);
+
+  record.timeOutEvents.push({
+    type: "TimeOut",
+    hour: parseInt(dateTime.split(" ")[1]),
+    date: dateTime.split(" ")[0]
+  });
+  return record;
+}
+
+function hoursWorkedOnDate(employeeRecord, date) {
+  const timeInEventOnDate = employeeRecord.timeInEvents.find(e => e.date === date);
+  const timeOutEventOnDate = employeeRecord.timeOutEvents.find(e => e.date === date);
+  return (timeOutEventOnDate.hour - timeInEventOnDate.hour) / 100;
+}
+
+function wagesEarnedOnDate(employeeRecord, date) {
+  const hoursWorked = hoursWorkedOnDate(employeeRecord, date);
+  return hoursWorked * employeeRecord.payPerHour;
+}
+
+function allWagesFor(employeeRecord) {
+  const wagesArray = employeeRecord.timeInEvents.map(e => {
+    return wagesEarnedOnDate(employeeRecord, e.date);
+  })
+
+  return wagesArray.reduce((acc, curr) => acc + curr);
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+  return srcArray.find(e => e.firstName === firstName);
+}
+
+function calculatePayroll(employeeRecords) {
+  const allEmployeeWages = employeeRecords.map(e => {
+    return allWagesFor(e);
+  })
+
+  return allEmployeeWages.reduce((acc, curr) => acc + curr);
+}
